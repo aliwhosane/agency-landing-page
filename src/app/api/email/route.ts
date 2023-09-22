2a88382caec45c3986e2e12195cd1fae26debd61
Ali Husen <a.husen21@gmail.com>
2023-09-22 15:05:46 +0530

    adding access token

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,10 +2,22 @@ import nodemailer from 'nodemailer';
 import { tapeataleCreds } from '../../../../constants';
 import Mail from 'nodemailer/lib/mailer';
 import {  type NextRequest,NextResponse} from 'next/server';
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 
 export async function POST(request:NextRequest) {
     const { email, name, message} = await request.json();
+
+    const oauth2Client = new OAuth2(
+       tapeataleCreds.gci,
+        tapeataleCreds.gcs,
+        "https://developers.google.com/oauthplayground" // Redirect URL
+   );
+   oauth2Client.setCredentials({
+    refresh_token: tapeataleCreds.grt
+});
+const accessToken = oauth2Client.getAccessToken()
     
     const transport = nodemailer.createTransport({
         service:'gmail',
@@ -15,6 +27,7 @@ export async function POST(request:NextRequest) {
         clientId: tapeataleCreds.gci,
         clientSecret: tapeataleCreds.gcs,
         refreshToken: tapeataleCreds.grt,
+        accessToken:accessToken
         }
     });
 
