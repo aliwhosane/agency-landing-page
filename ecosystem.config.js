e9deae917675815f1a0fc59b21d0e6dda698bc10
Ali Husen <a.husen21@gmail.com>
2023-10-08 14:28:55 +0530

    updated ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,15 @@ module.exports = {
 
   deploy: {
     production: {
-      key: "isayback.pem",
+      key: "tapeatale.pem",
       user: "ubuntu",
-      host: "13.212.34.159",
+      host: "13.233.74.10",
       ref: "origin/main",
       repo: "https://github.com/aliwhosane/agency-landing-page",
       path: "/home/ubuntu",
       "pre-deploy-local": "",
-      "post-deploy": "pm2 reload ecosystem.config.js --env production",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm run build && npm install && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
       ssh_options: "ForwardAgent=yes",
     },
