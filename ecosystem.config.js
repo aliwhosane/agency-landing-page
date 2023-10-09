3df70325aa3c809e8cb19e80e702aafed282c52b
Ali Husen <a.husen21@gmail.com>
2023-10-09 15:29:08 +0530

    new test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,14 +8,14 @@ module.exports = {
   deploy: {
     production: {
       key: "tapeatale.pem",
-      host: "13.233.74.10",
+      host: "65.0.11.232",
       user: "ubuntu",
-      ref: "origin/main",
+      ref: "origin/pm2",
       repo: "https://github.com/aliwhosane/agency-landing-page",
       path: "/home/ubuntu",
       "pre-deploy-local": "",
       "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && npm run build --node-args='--max-old-space-size=88192' && pm2 reload ecosystem.config.js --env production",
+        "source ~/.nvm/nvm.sh && npm install && npm run build --node-args='--max-old-space-size=4096' && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
       ssh_options: "ForwardAgent=yes",
     },
