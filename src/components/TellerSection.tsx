878691766dae81031710a5e78428d2057effc754
Ali Husen <a.husen21@gmail.com>
2023-10-15 12:09:07 +0530

    teller image updated

--- src/components/TellerSection.tsx
+++ src/components/TellerSection.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const TellerSection = () => {
   return (
-    <section className="dark:bg-gray-800 dark:text-gray-100">
+    <section className="">
       <div className="container flex flex-col-reverse mx-auto lg:flex-row">
-        <div className="lg:w-full xl:w-full dark:bg-gray-800">
-          <div className="flex items-center justify-center p-4 md:p-8 lg:p-12">
+        <div className="lg:w-full xl:w-full">
+          <div className="flex items-center justify-center md:p-8 lg:p-12">
             <img
-              src="all.png"
+              src="all.jpg"
               alt=""
-              className="rounded-lg shadow-lg dark:bg-gray-500 sm:min-h-96"
+              className="rounded-lg shadow-lg sm:min-h-96"
             />
           </div>
         </div>
