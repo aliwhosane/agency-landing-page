4a7dbc2dc20e89bde1bde6dc8d68c313883fcc2a
Ali Husen <a.husen21@gmail.com>
2023-10-20 16:02:46 +0530

    form token fix

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -3,6 +3,8 @@ import { FormData } from "@/components/ContactSection";
 export function sendMail(data:FormData) {
     const apiEndpoint ='/api/email';
 
+    console.log(data);
+
     fetch(apiEndpoint,{
         method:'POST',
         body:JSON.stringify(data),
